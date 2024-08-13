2829fb117d10a95482e00cd058ade7572b9bbb67
datdo <datvbhp123@gmail.com>
2024-08-13 17:54:04 +0700

    test commit

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -9,5 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call('UserSeeder');
+        $this->call('ProductsSeeder');
+        $this->call('ProductOptionsSeeder');
     }
 }
